230c89abf5a64a8a35f338b190aa3961dfa320c6
Zeynep Toy <zeynep@Hakan-MacBook-Pro.local>
2025-05-10 20:03:38 +0300

    Initial commit

---
+++ ARDrawing/ARDrawing/ARDrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARDrawingApp.swift
+//  ARDrawing
+//
+//  Created by Zeynep Toy on 10.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ARDrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
